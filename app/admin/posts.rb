9a4cb097608a41c4a33faef405aa65c67efe465b
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-05-10 19:53:08 +0300

    add category selection to new post form

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register Post do
   form do |f|
   f.inputs "Details", :multipart => true do
     f.input :title
+    f.input :category
   end
   f.inputs "Content" do
     f.input :content, :input_html => { :class => "ckeditor" }
