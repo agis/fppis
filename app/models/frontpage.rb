524870127cd7705c36d9da2196d9c9cb4ec277e2
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-05-15 21:30:55 +0300

    some notes

--- app/models/frontpage.rb
+++ app/models/frontpage.rb
@@ -1,6 +1,8 @@
 class Frontpage < ActiveRecord::Base
   attr_accessible :main, :first, :second, :third
 
+  # TODO: add validations
+  # TODO: ask Corey about styles
   has_attached_file :main, :styles => { :thumb => "125x125>" }
   has_attached_file :first, :styles => { :thumb => "125x125>" }
   has_attached_file :second, :styles => { :thumb => "125x125>" }
