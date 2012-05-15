6bd8f1a4b55b2739d2a5c0ffd46be0857fa98742
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-05-15 19:06:56 +0300

    fix ckeditor width to match the blogs content-area width and remove its label

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -8,14 +8,15 @@ ActiveAdmin.register Post do
       f.input :story_subtitle
       f.input :publish_date, label: 'Published Date'
       f.input :country
-      f.input :city, label: 'City / County'
+      f.input :city, label: 'City / Town'
       # TODO: make this a multi-select field (later)
       f.input :tag_list, label: 'Tags', hint: 'Comma separated'
     end
 
     f.inputs "Content" do
       # TODO: reduce ckeditor width to match the frontpage div's width
-      f.input :content, :input_html => { :class => "ckeditor" }
+      f.input :content, as: :ckeditor, label: false,
+                        input_html: { width: 640, height: 500 }
     end
 
    f.inputs "Credits" do
