ba90d402e223e99e6ad25cbfff0e6b5396165d05
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-05-19 18:20:10 +0300

    remove the uneeded Category Title field

--- db/migrate/20120511172732_add_fields_to_posts.rb
+++ db/migrate/20120511172732_add_fields_to_posts.rb
@@ -1,6 +1,5 @@
 class AddFieldsToPosts < ActiveRecord::Migration
   def change
-    add_column :posts, :category_title, :string
     add_column :posts, :story_title, :string
     add_column :posts, :story_subtitle, :string
   end
