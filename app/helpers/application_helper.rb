0d1a52ffcfcdd3c2ab4178a1edcf0d924725607d
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-05-19 21:00:01 +0300

    fix Blog#index layout and some more fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,6 @@ module ApplicationHelper
 
   def smart_truncate(s, opts = {})
     opts = {:words => 12}.merge(opts)
-    if opts[:sentences]
-      return s.split(/\.(\s|$)+/)[0, opts[:sentences]].map{|s| s.strip}.join('. ') + '.'
-    end
     a = s.split(/[ ]+/)
     n = opts[:words]
     a[0...n].join(' ') + (a.size > n ? '...' : '')
