7aeb77d218a89b39359b0844b48459d6c409b29a
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-05-30 13:20:27 +0300

    do not destroy images when a gallery is destroyed

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,7 +1,7 @@
 class Gallery < ActiveRecord::Base
   attr_accessible :title, :category_id, :country, :city
 
-  has_many :images, dependent: :destroy
+  has_many :images
   belongs_to :category
 
   validates :title, presence: true, uniqueness: true
