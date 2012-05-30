20c8e7fb4a0ddb517abf7d2a6f818f06f36df71e
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-05-30 13:26:01 +0300

    implement basic Gallery frontend functionality (show, filter by categories, layouts)

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,9 +1,11 @@
 class GalleriesController < ApplicationController
   def index
     @galleries = Gallery.all
+    @galleries = Gallery.where("category_id = ?", params[:c]) if params[:c]
   end
 
   def show
-    
+    @gallery = Gallery.find(params[:id])
+    @images = @gallery.images
   end
 end
