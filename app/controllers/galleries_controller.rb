416fcb24a62818f7cdf0fda737c9de1e2f767376
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-05-31 15:24:46 +0300

    implement gallery functionality frontend

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,11 +1,33 @@
 class GalleriesController < ApplicationController
   def index
-    @galleries = Gallery.all
-    @galleries = Gallery.where("category_id = ?", params[:c]) if params[:c]
+    set_images
   end
 
   def show
     @gallery = Gallery.find(params[:id])
     @images = @gallery.images
+  end
+
+  private
+
+  def set_images
+    @food_images = []
+    @people_images = []
+    @places_images = []
+
+    galleries = Gallery.where("category_id = 1").order("created_at DESC").limit(3)
+    galleries.each do |gallery|
+      @food_images << gallery.images.last unless gallery.images.empty?
+    end
+
+    galleries = Gallery.where("category_id = 2").order("created_at DESC").limit(3)
+    galleries.each do |gallery|
+      @people_images << gallery.images.last unless gallery.images.empty?
+    end
+
+    galleries = Gallery.where("category_id = 3").order("created_at DESC").limit(3)
+    galleries.each do |gallery|
+      @places_images << gallery.images.last unless gallery.images.empty?
+    end
   end
 end
