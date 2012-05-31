9ad66c0bed73d782be22ca848538e46095513d5e
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-05-31 15:38:08 +0300

    change in activeadmin

---
+++ app/admin/albums.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Album do
+  menu parent: "Image Galleries"
+  actions :all, :except => [:show]
+
+  index do
+    column :id
+    column :title
+    column :country
+    column :city
+    column "No. of Images" do |gallery|
+      gallery.images.count
+    end
+    column :category
+
+    default_actions
+  end
+end
