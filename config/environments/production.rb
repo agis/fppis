10d0208fa77e067d6ae5eabbc3985b453638a99c
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-06-07 17:42:39 +0300

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Fppis::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -45,9 +45,6 @@ Fppis::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-
-  
-
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
