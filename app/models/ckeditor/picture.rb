55dcd8a2dbd293efd79e37aadb97fb56ca290bf4
Agis Anastasopoulos <panagiotis@core-studios.net>
2012-06-08 12:35:38 +0300

    fix ckeditor post attachment

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -9,8 +9,4 @@ class Ckeditor::Picture < Ckeditor::Asset
 
 	validates_attachment_size :data, :less_than => 2.megabytes
 	validates_attachment_presence :data
-
-	def url_content
-	  url(:content)
-	end
 end
